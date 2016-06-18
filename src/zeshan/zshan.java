582c07d1916c0ec9bbd95dd32a55e94a839e7051
Calypso Computers <Calypso Computers@192.168.10.17>
2016-06-19 01:45:37 +0500

    # WARNING: head commit changed in the meantime
    fff

--- src/zeshan/zshan.java
+++ src/zeshan/zshan.java
@@ -8,6 +8,7 @@ public class zshan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("hi welcom to java");
+System.out.print("hi welcom to java");
 	}
 
 }
